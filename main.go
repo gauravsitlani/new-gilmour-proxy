d874486c9db15a35d511d0504565552f0fe5abb9
Gaurav Sitlani <gauravsitlani@riseup.net>
2018-04-05 14:29:10 +0530

    proxy modified along with the test service

--- main.go
+++ main.go
@@ -14,23 +14,29 @@ import (
 func createNodeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		body, err := ioutil.ReadAll(r.Body)
+		log.Println(string(body))
 		if err != nil {
-			fmt.Fprintf(w, "Error : %s!", proxy.LogError(err))
+			fmt.Fprintf(w, "Error : %s!", err)
+			return
 		}
 		nodeReq := new(proxy.NodeReq)
 		if err = json.Unmarshal(body, nodeReq); err != nil {
-			fmt.Fprintf(w, "Error : %s ", proxy.LogError(err))
+			fmt.Fprintf(w, "Error : %s ", err)
+			return
 		}
 		engine, err := proxy.MakeGilmour("127.0.0.1:6379")
 		if err != nil {
-			fmt.Fprintf(w, "Error : %s!", proxy.LogError(err))
+			fmt.Fprintf(w, "Error : %s!", err)
+			return
 		}
 		node, err := proxy.CreateNode(nodeReq, engine)
 		if err != nil {
-			fmt.Fprintf(w, "Error : %s!", proxy.LogError(err))
+			fmt.Fprintf(w, "Error : %s!", err)
+			return
 		}
 		if err = node.Start(); err != nil {
-			fmt.Fprintf(w, "Error : %s!", proxy.LogError(err))
+			fmt.Fprintf(w, "Error : %s!", err)
+			return
 		}
 
 		go proxy.NodeWatchdog(node)
@@ -38,7 +44,8 @@ func createNodeHandler(w http.ResponseWriter, r *http.Request) {
 		response := node.FormatResponse()
 		js, err := json.Marshal(response)
 		if err != nil {
-			fmt.Fprintf(w, "Error : %s!", proxy.LogError(err))
+			fmt.Fprintf(w, "Error : %s!", err)
+			return
 		}
 		w.Header().Set("Content-Type", "application/json")
 		if _, err = w.Write(js); err != nil {
@@ -59,7 +66,7 @@ func getNode(id string) (node *proxy.Node, err error) {
 func formatResponse(key string, value interface{}) interface{} {
 	js, err := json.Marshal(map[string]interface{}{key: value})
 	if err != nil {
-		proxy.LogError(err)
+		log.Println(err)
 		js = []byte(err.Error())
 	}
 	return js
@@ -85,10 +92,12 @@ func deleteNodeHandler(w http.ResponseWriter, req *http.Request) {
 		logWriterError(w, err)
 		return
 	}
+
 	if err = proxy.DeleteNode(node); err != nil {
 		logWriterError(w, err)
 		return
 	}
+
 	js := formatResponse("status", "ok")
 	w.Header().Set("Content-Type", "application/json")
 	if _, err = w.Write(js.([]byte)); err != nil {
@@ -97,13 +106,37 @@ func deleteNodeHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 
+// GET /nodes/{id} getting details of an existing node
+func getServicesHandler(w http.ResponseWriter, req *http.Request) {
+	vars := mux.Vars(req)
+	id := vars["id"]
+	node, err := getNode(id)
+	if err != nil {
+		logWriterError(w, err)
+		return
+	}
+	response, err := node.GetServices()
+	if err != nil {
+		logWriterError(w, err)
+		return
+	}
+	js := formatResponse("services", response)
+	w.Header().Set("Content-Type", "application/json")
+	if _, err = w.Write(js.([]byte)); err != nil {
+		//	log.Println(err.EdHat RHCE, RHCSArror())
+	}
+	return
+}
+
 func main() {
+	proxy.InitNodeMap()
+
 	r := mux.NewRouter()
 	log.Println("listening...")
 	r.HandleFunc("/nodes", createNodeHandler)
 	r.HandleFunc("/nodes/{id}", deleteNodeHandler).Methods("DELETE")
-	http.Handle("/", r)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	r.HandleFunc("/nodes/{id}",getServicesHandler).Methods("GET")
+	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Println(err.Error())
 	}
 }
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -16,13 +16,6 @@ import (
 	"time"
 )
 
-// LogError prints error on console, returns string format of the error and raises a panic
-func LogError(err error) string {
-	// panic(err)
-	log.Println(err.Error())
-	return err.Error()
-
-}
 
 type Status int
 
@@ -38,9 +31,18 @@ func initNodeMap() (nm *nodeMap) {
 	return
 }
 
-var (
-	nMap = initNodeMap()
-)
+var nMap *nodeMap
+
+func InitNodeMap() {
+	if nMap != nil {
+		return
+	}
+
+	nMap = new(nodeMap)
+	nMap.regNodes = make(map[NodeID]*Node)
+
+	log.Println("Node Map Initialized")
+}
 
 // NodeMapOperations is a interface to enable operation on nodeMap
 type NodeMapOperations interface {
@@ -57,27 +59,31 @@ func GetNodeMap() nodeMap {
 // Put adds node in nodeMap
 func (n *nodeMap) Put(uid NodeID, node *Node) (err error) {
 	n.Mutex.Lock()
+	defer n.Mutex.Unlock()
 	n.regNodes[uid] = node
-	n.Mutex.Unlock()
+
 	return
 }
 
 // Del removes node from nodeMap
 func (n *nodeMap) Del(uid NodeID) (err error) {
 	n.Mutex.Lock()
+	defer n.Mutex.Unlock()
 	delete(n.regNodes, uid)
-	n.Mutex.Unlock()
+
 	return
 }
 
 // Get returns node from nodeMap
 func (n *nodeMap) Get(uid NodeID) (node *Node, err error) {
 	n.Mutex.Lock()
+	defer n.Mutex.Unlock()
 	node = n.regNodes[uid]
-	n.Mutex.Unlock()
+
 	if node == nil {
 		err = errors.New("Node not found")
 	}
+
 	return
 }
 
@@ -107,7 +113,6 @@ type Message struct {
 	Data        interface{} `json:"data"`
 	HandlerPath string      `json:"handler_path"`
 }
-
 
 
 type GilmourTopic string
@@ -186,24 +191,37 @@ func (node *Node) GetEngine() *G.Gilmour {
 	return node.engine
 }
 
+// GetServices returns all the services which node is currently subscribed to
+func (node *Node) GetServices() (services ServiceMap, err error) {
+	if node.status == 200 {
+		services = node.services
+	}
+	return
+}
+
 // Stop Exit routine. UnSubscribes Slots, removes registered health ident and triggers backend Stop
 func (node *Node) Stop() (err error) {
 	node.engine.Stop()
 	return
 }
 
-func DeleteNode(node *Node) (err error){
-	_, err = http.Get("http://127.0.0.1:" + node.port)
-	if err != nil{
-		LogError(err)
+func DeleteNode(node *Node) (error){
+	if _, err := http.Get("http://127.0.0.1:" + node.port); err != nil {
+		log.Println(err)
+		return err
 	}
-	if err = nMap.Del(node.id); err!= nil {
-		LogError(err)
+
+	if err := nMap.Del(node.id); err!= nil {
+		log.Println(err)
+		return err
 	}
-	if err = node.Stop(); err != nil {
-		LogError(err)
+
+	if err := node.Stop(); err != nil {
+		log.Println(err)
+		return err
 	}
-	return
+
+	return nil
 }
 
 //******************************************************************************
@@ -212,7 +230,7 @@ func DeleteNode(node *Node) (err error){
 type CreateNodeResponse struct {
 	ID            string `json:"id"`
 	PublishPort   string `json:"publish_port"`
-	Status        string `json:"status"`
+	Status        int `json:"status"`
 }
 
 
@@ -233,7 +251,7 @@ func uniqueNodeID(strlen int) (id string) {
 func (node *Node) FormatResponse() (resp CreateNodeResponse) {
 	resp.ID = string(node.id)
 	resp.PublishPort = node.port
-	resp.Status = fmt.Sprintf("%s", node.status)
+	resp.Status,_ = fmt.Printf("%v", node.status)
 	return
 }
 
@@ -287,7 +305,7 @@ func (node *Node) AddService(topic GilmourTopic, service Service) (err error) {
 func (node *Node) AddServices(services ServiceMap) (err error) {
 	for topic, service := range services {
 		if err = node.AddService(topic, service); err != nil {
-			LogError(err)
+			log.Println(err)
 			return
 		}
 	}
@@ -296,41 +314,52 @@ func (node *Node) AddServices(services ServiceMap) (err error) {
 
 
 // Start will Start Gilmour engine and added services of slots if any in the Node struct instance
-func (node *Node) Start() (err error) {
+func (node *Node) Start() (error) {
+	if node == nil {
+		log.Println("NODE is nill")
+	}
 	if node.engine == nil {
 		return errors.New("Please setup backend engine")
 	}
 	node.engine.Start()
-	if err = node.AddServices(node.services); err != nil {
-		return
+	if err := node.AddServices(node.services); err != nil {
+		return err
 	}
-	return
+	return nil
 }
 
 //Getting status of Node and running it
 
-func (node *Node) GetStatus(sync bool)(status Status,err error){
-	addr := "localhost"
-	addr_port := node.port
-	addr = addr + addr_port
-	_, err = http.Get(addr)
+func (node *Node) GetStatus(sync bool)(Status, error){
+
+	addr := fmt.Sprintf("http://127.0.0.1:%s", node.port)
+	log.Println(addr)
+	resp, err := http.Get(addr)
 	//log.Println(res.Body)
 	if err != nil {
-		node.status = 404 //dirty
+		log.Println(err)
+		node.status = 500 //dirty
 		return node.status, err
 	}
+
 	hlp := "/health_check"
 	addr = addr + hlp
-	req,err := http.Get(addr)
+	req, err := http.Get(addr)
 	
 	if err != nil {
-		node.status = 403 //unavailable
+		node.status = Status(req.StatusCode) //unavailable
 	}
+	defer resp.Body.Close()
+
 	body ,err := ioutil.ReadAll(req.Body)
 	if err != nil{
 		log.Println(err)
+		return 0, err
 	}
+
 	reply := string(body)
+
+	// TODO: validate these assumptions
 	if reply == "OK"{
 		node.status = 200 //available //ok
 	} else {
@@ -351,10 +380,14 @@ func NodeWatchdog(node *Node) {
 	stopped := false
 	for {
 		<-time.After(time.Second * 10)
+
 		status, err := node.GetStatus(true)
 		if err != nil {
 			log.Println(err.Error())
+			// Notify to stakeholders
+			continue
 		}
+
 		if status == 403 && !stopped {
 			stopped = true
 			if err = node.Stop(); err != nil {
@@ -384,15 +417,26 @@ func MakeGilmour(connect string) (engine *G.Gilmour, err error) {
 	return
 }
 
-func CreateNode(nodeReq *NodeReq, engine *G.Gilmour) (node *Node, err error){
-	node = new(Node)
+func CreateNode(nodeReq *NodeReq, engine *G.Gilmour) (*Node, error){
+	node := new(Node)
 	node.engine = engine
 	node.id = NodeID(uniqueNodeID(50))
 	node.healthcheckpath = nodeReq.HealthCheckPath
 	node.port = nodeReq.Port
 	node.services = make(ServiceMap)
 	node.services = nodeReq.Services
+
+	if node.engine == nil {
+		log.Println("Engine is nil")
+	}
+
+	var err error
 	node.status, err = node.GetStatus(true)
+	if err != nil {
+		log.Printf("Cannot get status of a node: %+v", err)
+		return nil, err
+	}
+
 	err = nMap.Put(node.id,node)
-	return
+	return node, nil
 }
